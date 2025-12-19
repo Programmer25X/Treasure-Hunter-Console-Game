07ae97867b93ed4a42e9190f76e49c4b027002ac
Programmer25X <denniscullipcross@icloud.com>
2025-12-19 17:01:45 +0000

    Started adding levels

--- Characters.cpp
+++ Characters.cpp
@@ -1,4 +1,5 @@
 #include "Characters.h"
+#include "Game.h"
 #include "Items.h"
 
 extern int NUMBER_OF_COLUMNS;
@@ -128,6 +129,7 @@ void PlayerCharacter::move(int keycode)
 	int newXCoordinate = -1;
 	int newYCoordinate = -1;
 
+
 	switch (keycode)
 	{
 	case 87: // W
@@ -148,8 +150,17 @@ void PlayerCharacter::move(int keycode)
 		break;
 	}
 
-	if (newXCoordinate <= 0 || newXCoordinate >= NUMBER_OF_COLUMNS || newYCoordinate <= 0 || newYCoordinate >= NUMBER_OF_ROWS)
+
+	if (newXCoordinate <= 0 || newXCoordinate >= NUMBER_OF_COLUMNS - 1 || newYCoordinate <= 0 || newYCoordinate >= NUMBER_OF_ROWS - 1)
 	{
+		for (Item* wall : walls)
+		{
+			if (wall->getIsInteractable() && wall->getXCoordinate() == newXCoordinate && wall->getYCoordinate() == newYCoordinate)
+			{
+				return;
+			}
+		}
+
 		return;
 	}
 	else
--- Game.cpp
+++ Game.cpp
@@ -446,3 +446,4 @@ void Game::updateGame()
 
 
 
+
